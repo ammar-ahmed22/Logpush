a0e74147faa44bee50ded80673280855e990d143
Ammar Ahmed <ammar@ammar.lan>
2022-06-19 13:21:19 -0400

    hello world get

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.status(200).send({
+        "message": "Hello World!"
+    })
+})
+
 app.post("/logpush", (req, res) => {
     
     console.log(req)
